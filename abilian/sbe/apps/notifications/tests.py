5b98db128c4d7d1960ca46e98e021725247f19d8
Bertrand Mathieu <bmathieu@abilian.com>
2015-03-11 11:11:50 +0100

    notifications: fix mail template + test rendering

--- abilian/sbe/apps/notifications/tests.py
+++ abilian/sbe/apps/notifications/tests.py
@@ -3,13 +3,17 @@
 """
 from __future__ import absolute_import
 
+from flask import render_template
+
 from abilian.core.models.subjects import User
 from abilian.web import url_for
 
 from abilian.sbe.testing import BaseTestCase
-from abilian.sbe.apps.notifications.tasks.social import \
-     generate_unsubscribe_token
-
+from abilian.sbe.apps.communities.tests.base import CommunityBaseTestCase
+from abilian.sbe.apps.communities.models import WRITER
+from abilian.sbe.apps.notifications.tasks.social import (
+     CommunityDigest, generate_unsubscribe_token,
+)
 
 class TestNotificationViews(BaseTestCase):
 
@@ -19,6 +23,7 @@ class TestNotificationViews(BaseTestCase):
     # if he's not authentified
     cfg.CSRF_ENABLED = True
     cfg.WTF_CSRF_ENABLED = True
+    cfg.NO_LOGIN = False
     return cfg
 
   def setUp(self):
@@ -43,3 +48,25 @@ class TestNotificationViews(BaseTestCase):
     self.assert_200(r)
     prefs = preferences.get_preferences(self.user)
     self.assertFalse(prefs['sbe:notifications:daily'])
+
+
+class NotificationTestCase(CommunityBaseTestCase):
+
+  def setUp(self):
+    super(NotificationTestCase, self).setUp()
+    self.user = User(email=u'user_1@example.com', password='azerty',
+                     can_login=True)
+    self.session.add(self.user)
+    self.community.set_membership(self.user, WRITER)
+    self.session.commit()
+
+
+  def test_mail_templates(self):
+    # this actually tests that templates are parsed without errors, not the
+    # rendered content
+    digests = [CommunityDigest(self.community)]
+    token = generate_unsubscribe_token(self.user)
+    render_template("notifications/daily-social-digest.txt",
+                    digests=digests, token=token)
+    render_template("notifications/daily-social-digest.html",
+                    digests=digests, token=token)
