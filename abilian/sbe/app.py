d617f1598c2461e27d7efa06db184822bd5183c4
Bertrand Mathieu <bmathieu@abilian.com>
2015-03-27 13:06:36 +0100

    app.py: add setup_command for easy app bootstrap
    run with python -m abilian.sbe.app setup_sbe_app

--- abilian/sbe/app.py
+++ abilian/sbe/app.py
@@ -7,10 +7,14 @@ and activation.
 """
 from __future__ import absolute_import
 
+import os, sys, subprocess
 import logging
+from pathlib import Path
 
 import jinja2
-from flask.ext.script import Manager
+from werkzeug.serving import BaseWSGIServer
+from flask import current_app
+from flask_script import Manager, Command
 
 from abilian.app import Application as BaseApplication
 from abilian.core.extensions import db
@@ -31,7 +35,11 @@ __all__ = ['create_app', 'db']
 logger = logging.getLogger(__name__)
 
 def create_app(config=None):
-  return Application(config=config)
+  return Application(name='abilian_sbe', config=config)
+
+
+command_manager = Manager(create_app)
+setup_abilian_commands(command_manager)
 
 # loader to be used by celery workers
 class CeleryLoader(CeleryBaseLoader):
@@ -57,7 +65,7 @@ class Application(BaseApplication):
     "abilian.sbe.apps.preferences",
   )
 
-  script_manager = '.app.command_manager'
+  script_manager = command_manager
 
   def __init__(self, name='abilian.sbe', config=None, **kwargs):
     BaseApplication.__init__(self, name, config=config, **kwargs)
@@ -72,12 +80,94 @@ class Application(BaseApplication):
     converter.init_app(self)
 
 
-command_manager = Manager(create_app)
-setup_abilian_commands(command_manager)
+# SBE demo app bootstrap stuff
+_SBE_DEMO_SCRIPT = u'''\
+#!{BIN_DIR}/python
+from __future__ import absolute_import
+
+import sys
+from abilian.sbe.app import command_entry_point
+
+sys.exit(command_entry_point())
+'''
+
+
+_BASE_SERVER_ACTIVATE = BaseWSGIServer.server_activate
+
+
+def _on_http_server_activate(self, *args, **kwargs):
+  """
+  This function is used as to monkey patch BaseWSGIServer.server_activate
+  during `setup_sbe_demo`.
+  """
+  _BASE_SERVER_ACTIVATE(self, *args, **kwargs)
+  # now we are listening to socket
+  host, port = self.server_address
+  if host == u'0.0.0.0':
+    # chrome is not ok with 0.0.0.0
+    host = u'localhost'
+  url = 'http://{host}:{port}/setup'.format(host=host, port=port)
+
+  if sys.platform == "win32":
+    os.startfile(url)
+  else:
+    opener ="open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, url])
+
+
+# run with python -m abilian.sbe.app setup_sbe_app
+def setup_sbe_app():
+  """
+  Basic set up SBE application. Must be run inside a virtualenv.
+
+  Will create `abilian_sbe` script, run a local server and open browser on app's
+  setup wizard.
+  """
+  logger = logging.getLogger('sbe_demo')
+  logger.setLevel(logging.INFO)
+
+  if 'VIRTUAL_ENV' not in os.environ:
+    logger.error('Not in a virtualenv! Aborting.')
+    return 1
+
+  bin_dir = Path(sys.prefix) / u'bin'
+
+  if not bin_dir.exists() and bin_dir.is_dir():
+    logger.error('%s doesn\'t exists or is not a directory. Aborting',
+                 repr(unicode(bin_dir)))
+    return 1
+
+  script_file = bin_dir / u'abilian_sbe'
+  if script_file.exists():
+    logger.info('%s already exists. Skipping creation.',
+                repr(unicode(script_file)))
+  else:
+    with script_file.open('w') as out:
+      logger.info('Create script: "%s".', repr(unicode(script_file)))
+      content = _SBE_DEMO_SCRIPT.format(BIN_DIR=unicode(bin_dir))
+      out.write(content)
+    script_file.chmod(0755) # 0755: -rwxr-xr-x
+
+  current_app.config['PRODUCTION'] = True
+  current_app.config['DEBUG'] = False
+  current_app.config['ASSETS_DEBUG'] = False
+  current_app.config['SITE_NAME'] = u'Abilian SBE'
+  current_app.config['MAIL_SENDER'] = u'abilian-sbe-app@example.com'
+
+  logger.info('Prepare CSS & JS files')
+  command_manager.handle('abilian_sbe', ['assets', 'build'])
+  # disabled init config: only if not running setupwizard
+  # command_manager.handle('abilian_sbe', ['config', 'init'])
+
+  # patch server used to launch browser here immediately after socket opened
+  BaseWSGIServer.server_activate = _on_http_server_activate
+  return command_manager.handle('abilian_sbe', ['run', '--hide-config'])
 
 
 def command_entry_point():
-  command_manager.run()
+  command_manager.run(
+      commands={'setup_sbe_app': Command(setup_sbe_app)},
+  )
 
 
 if __name__ == '__main__':
