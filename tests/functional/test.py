4a4a0514579b2fd4e4355a581f68110097916c1e
yvon <redyvon@gmail.com>
2015-04-23 18:54:06 +0200

    tests/functional  port is now dynamic to avoid runtime errors

--- tests/functional/test.py
+++ tests/functional/test.py
@@ -8,25 +8,52 @@ Also, we're using pytest-splinter
 (https://github.com/pytest-dev/pytest-splinter) to inject the browser as
 a pytest fixture.
 """
+import socket
 import tempfile
 import shutil
 import multiprocessing
 from pathlib import Path
 import pytest
 import pytest_splinter.plugin  # noqa
+from werkzeug.serving import select_ip_version
 
 from abilian.sbe.app import Application
 
 
-# TODO: switch to another port if this one is not available?
-PORT = 5000
-ROOT = "http://localhost:{}".format(PORT)
+@pytest.fixture(scope='session')
+def app_port(request):
+  port = 30000
+  hostname = '127.0.0.1'
+
+  address_family = select_ip_version(hostname, port)
+  test_socket = socket.socket(address_family, socket.SOCK_STREAM)
+  test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  while port < 60000:
+    try:
+      test_socket.bind((hostname, port))
+      test_socket.close()
+
+    except socket.error as exc:
+      if exc.errno != 98:
+        # errno(98, Address already in use)
+        raise
+      port +=1
+
+    break
+
+  return port
+
+
+@pytest.fixture(scope='session')
+def app_root(app_port):
+  return "http://localhost:{}".format(app_port)
 
 
 # We're using phantomjs as our default browser.
 @pytest.fixture(scope='session')  # pragma: no cover
 def splinter_webdriver(request):
   return 'phantomjs'
+
 
 @pytest.fixture(scope='module')
 def instance_path(request):
@@ -39,9 +66,11 @@ def instance_path(request):
     shutil.rmtree(tmp_dir)
 
   request.addfinalizer(clear)
+  return tmp_dir
+
 
 @pytest.fixture(scope='module')
-def app(request, instance_path):
+def app(request, instance_path, app_port):
   app = Application(instance_path=instance_path)
 
   # FIXME: need a working environment (DB, Redis...)
@@ -51,7 +80,7 @@ def app(request, instance_path):
   def worker(app, port):
     app.run(port=port)
 
-  process = multiprocessing.Process(target=worker, args=(app, PORT))
+  process = multiprocessing.Process(target=worker, args=(app, app_port))
 
   try:
     process.start()
@@ -67,12 +96,12 @@ def app(request, instance_path):
   return app
 
 
-def test_home(browser, app):
-  browser.visit(ROOT)
+def test_home(browser, app, app_root):
+  browser.visit(app_root)
 
 
-def test_login(browser, app):
-  browser.visit(ROOT + '/user/login')
+def test_login(browser, app, app_root):
+  browser.visit(app_root + '/user/login')
   browser.fill('email', "admin@example.com")
   browser.fill('password', "admin")
   button = browser.find_by_xpath("//form[@name='login']//button")
@@ -83,5 +112,5 @@ def test_login(browser, app):
   # assert "Welcome to Abilian" in browser.find_element_by_xpath("/html/body").text
 
 
-def test_forgotten_pw(browser, app):
-  browser.visit(ROOT + '/user/forgotten_pw')
+def test_forgotten_pw(browser, app, app_root):
+  browser.visit(app_root + '/user/forgotten_pw')
