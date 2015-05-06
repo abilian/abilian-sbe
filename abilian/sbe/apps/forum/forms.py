58424d7c73f2934200d7674c7be4b955b2667dc6
Bertrand Mathieu <bmathieu@abilian.com>
2015-05-06 12:36:02 +0200

    forum: form factorisation

--- abilian/sbe/apps/forum/forms.py
+++ abilian/sbe/apps/forum/forms.py
@@ -4,8 +4,9 @@
 from __future__ import absolute_import
 
 import bleach
-from flask.ext.babel import lazy_gettext as _l
 from wtforms import TextAreaField, StringField, BooleanField
+
+from abilian.i18n import _l
 from abilian.web.forms import Form, RichTextWidget
 from abilian.web.forms.validators import required, optional
 from abilian.web.forms.filters import strip
@@ -51,22 +52,11 @@ for attr in ALLOWED_TAGS:
   WIDGET_ALLOWED[attr] = allowed
 
 
-class ThreadForm(Form):
-  title = StringField(label=_l(u"Title"),
-                      filters=(strip,),
-                      validators=[required()])
-  message = TextAreaField(label=_l(u"Message"),
-                          widget=RichTextWidget(allowed_tags=WIDGET_ALLOWED),
+# instantiate this one before CommentForm fields, so that it is listed first
+# when Threadform is displayed
+_TITLE_FIELD = StringField(label=_l(u"Title"),
                           filters=(strip,),
                           validators=[required()])
-  attachments = FileField(label=_l(u'Attachments'), multiple=True,
-                          validators=[optional()])
-  send_by_email = BooleanField(label=_l(u"Send by email?"), default=True)
-
-  def validate_message(self, field):
-    field.data = bleach.clean(field.data, tags=ALLOWED_TAGS,
-                              attributes=ALLOWED_ATTRIBUTES, strip=True)
-
 
 class CommentForm(Form):
   message = TextAreaField(label=_l("Message"),
@@ -80,3 +70,7 @@ class CommentForm(Form):
   def validate_message(self, field):
     field.data = bleach.clean(field.data, tags=ALLOWED_TAGS,
                               attributes=ALLOWED_ATTRIBUTES, strip=True)
+
+
+class ThreadForm(CommentForm):
+  title = _TITLE_FIELD
