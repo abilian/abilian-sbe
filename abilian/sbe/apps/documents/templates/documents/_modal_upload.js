0836d593fa6eea357e87fb8a713fe2883acc7a00
Bertrand Mathieu <bmathieu@abilian.com>
2015-06-02 16:57:46 +0200

    update JS code to use require()

---
+++ abilian/sbe/apps/documents/templates/documents/_modal_upload.js
@@ -0,0 +1,63 @@
+(function() {
+function setupUpload(Abilian, $, FileAPI) {
+    var form_data = {action: "upload"};
+    form_data[Abilian.csrf_fieldname] = Abilian.csrf_token;
+
+    function onUploadComplete(evt, ui) {
+        var type = evt.type,
+            uid = FileAPI.uid(ui.file),
+            widget = $(this).fileapi('widget'),
+            $file_ui = widget.$file(uid),
+            $status_el = $file_ui.find('.statusicon'),
+            $icon = $status_el.find('.glyphicon');
+
+        if (type == 'fileupload') {
+            $icon.addClass('glyphicon-upload');
+            $status_el.removeClass('hidden');
+        }
+        else if (type == 'filecomplete') {
+            $icon.removeClass('glyphicon-upload');
+            $icon.addClass('glyphicon-ok');
+            $status_el.removeClass('btn-default');
+            $status_el.addClass('btn-success');
+            $file_ui.find('.progress .progress-bar').width('100%');
+        }
+    }
+
+    $('#modal-upload-files')
+        .fileapi({
+            multiple: true,
+            url: {{ folder_post_url|tojson }},
+            data: form_data,
+            dataType: null, {# not JSON #}
+            clearOnComplete: false,
+            elements: {
+                ctrl: { upload: '.js-upload' },
+                empty: { show: '.b-upload__hint' },
+                emptyQueue: { hide: '.js-upload' },
+                list: '.js-files',
+                file: {
+                    tpl: '.js-file-tpl',
+                    preview: {
+                        el: '.b-thumb__preview',
+                        width: 40,
+                        height: 40
+                    },
+                    upload: { hide: '.b-thumb__rotate' },
+                    progress: '.progress .progress-bar',
+                    complete: { }
+                }
+            },
+            onComplete: function (evt, ui) { location.reload(); }
+        })
+        .on('fileupload.fileapi filecomplete.fileapi', onUploadComplete)
+        .on('hidden.bs.modal', function(evt) {
+            /* clear file selection when modal is hidden (cancel button) */
+            $(this).fileapi('widget').clear();
+        });
+
+    form_data = null;
+}
+
+ require(['Abilian', 'jquery', 'FileAPI'], setupUpload);
+}());
--- abilian/sbe/apps/documents/templates/documents/_modals.js
+++ abilian/sbe/apps/documents/templates/documents/_modals.js
@@ -1,46 +1,119 @@
 /* jshint camelcase: false */
-(function($) {
+(function() {
     'use strict';
 
- var setup_modal_folder_inputname_check  = function(modal, object_id, action) {
-     var $submit = modal.find('button.btn-primary');
-     var $input = modal.find('input[name="title"]');
-     var $help_span = $input.next('span.help-block');
-     var $control_group = $input.closest('div.form-group');
+function setupFolderModal(Abilian, $) {
+    function setupModalFolderInputnameCheck(modal, object_id, action) {
+        var $submit = modal.find('button.btn-primary'),
+            $input = modal.find('input[name="title"]'),
+            $help_span = $input.next('span.help-block'),
+            $control_group = $input.closest('div.form-group');
 
-     $submit.on('click', function(e) {
-                    var title = $input.val();
-                    $.ajax('{{ url_for(".check_valid_name", community_id=g.community.slug) }}',
-                           { async: false,
-                             cache: false,
-                             data: { object_id: object_id,
-                                     title: title,
-                                     action: action },
-                             success: function(data) {
-                                 var valid = data.valid;
-                                 if (!valid) {
-                                     e.preventDefault();
-                                     $control_group.addClass('has-error');
-                                     $help_span.text(data.help_text);
-                                     $help_span.removeClass('hide');
-                                 } else {
-                                     $control_group.removeClass('has-error');
-                                     $help_span.text('');
-                                     $help_span.addClass('hide');
-                                 }
-                             }
-                           });
+        $submit.on('click', function(e) {
+            var title = $input.val();
+            $.ajax(
+                '{{ url_for(".check_valid_name", community_id=g.community.slug) }}',
+                { async: false,
+                  cache: false,
+                  data: { object_id: object_id,
+                          title: title,
+                          action: action },
+                  success: function(data) {
+                      var valid = data.valid;
+                      if (!valid) {
+                          e.preventDefault();
+                          $control_group.addClass('has-error');
+                          $help_span.text(data.help_text);
+                          $help_span.removeClass('hide');
+                      } else {
+                          $control_group.removeClass('has-error');
+                          $help_span.text('');
+                          $help_span.addClass('hide');
+                      }
+                  }
                 });
- };
+        });
+    };
+
+    // Simple document viewer...
+    function setupDocumentViewer() {
+        var imgSrc = $("img.preview").attr("src"),
+            previewNav = $(".preview-nav"),
+            previewPrev = $('.preview-prev'),
+            previewNext = $('.preview-next');
+
+        function showNav() {
+            $(document).bind("keydown", keyDown);
+            previewNav.stop().fadeTo(150, 1);
+        }
+
+        function hideNav() {
+            $(document).unbind("keydown", keyDown);
+            previewNav.stop().fadeTo(150, 0);
+        }
+
+        var page_num = {{ doc.page_num }};
+        if (page_num > 1) {
+            $("div.preview").hover(showNav, hideNav);
+        }
+
+        // TODO: what if we want to go past the last page?
+        function loadNext() {
+            var img = $("img.preview");
+            var page = img.data("page") + 1;
+            if (page >= page_num) {
+                page = page - 1;
+            }
+            img.attr("src", imgSrc + "&page=" + page);
+            img.data("page", page);
+            return true;
+        }
+
+        function loadPrev() {
+            var img = $("img.preview");
+            var page = img.data("page") - 1;
+            if (page < 0) {
+                page = 0;
+            }
+            img.attr("src", imgSrc + "&page=" + page);
+            img.data("page", page);
+            return true;
+        }
+
+        previewNext.click(loadNext);
+        previewPrev.click(loadPrev);
+
+        function keyDown(e) {
+            var code = e.keyCode;
+            // Note: we prevent default keyboard action
+            if (code == 39) {
+                loadNext();
+                return false;
+            } else if (code == 37) {
+                loadPrev();
+                return false;
+            }
+            return true;
+        }
+    }
 
     Abilian.fn.onAppInit(function() {
-        setup_modal_folder_inputname_check($('#modal-edit'),
-                                           '{{ doc.id if doc else folder.id }}',
-                                           '{%- if doc %}document{% else %}folder{% endif %}-edit');
-        setup_modal_folder_inputname_check($('#modal-new-folder'), '{{ folder.id }}', 'new');
+        setupModalFolderInputnameCheck(
+            $('#modal-edit'),
+            '{{ doc.id if doc else folder.id }}',
+            '{%- if doc %}document{% else %}folder{% endif %}-edit'
+        );
+        setupModalFolderInputnameCheck(
+            $('#modal-new-folder'),
+            '{{ folder.id }}',
+            'new'
+        );
 
         $('#modal-edit input[type="text"]').preventEnterKey();
         $('#modal-new-folder input[type="text"]').preventEnterKey();
-    });
 
-}(jQuery));
+        setupDocumentViewer();
+    });
+}
+    require(['Abilian', 'jquery'], setupFolderModal);
+}());
