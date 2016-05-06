07a86d4fdd836ba79cd6746589e62b0353e714ca
Stefane Fermigier <sf@fermigier.com>
2016-05-06 19:36:03 +0200

    Format tests.

--- tests/functional/test.py
+++ tests/functional/test.py
@@ -22,37 +22,37 @@ from abilian.sbe.app import Application
 
 @pytest.fixture(scope='session')
 def app_port(request):
-  port = 30000
-  hostname = '127.0.0.1'
+    port = 30000
+    hostname = '127.0.0.1'
 
-  address_family = select_ip_version(hostname, port)
-  test_socket = socket.socket(address_family, socket.SOCK_STREAM)
-  test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  while port < 60000:
-    try:
-      test_socket.bind((hostname, port))
-      test_socket.close()
+    address_family = select_ip_version(hostname, port)
+    test_socket = socket.socket(address_family, socket.SOCK_STREAM)
+    test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    while port < 60000:
+        try:
+            test_socket.bind((hostname, port))
+            test_socket.close()
 
-    except socket.error as exc:
-      if exc.errno != 98:
-        # errno(98, Address already in use)
-        raise
-      port +=1
+        except socket.error as exc:
+            if exc.errno != 98:
+                # errno(98, Address already in use)
+                raise
+            port += 1
 
-    break
+        break
 
-  return port
+    return port
 
 
 @pytest.fixture(scope='session')
 def app_root(app_port):
-  return "http://localhost:{}".format(app_port)
+    return "http://localhost:{}".format(app_port)
 
 
 # We're using phantomjs as our default browser.
 @pytest.fixture(scope='session')  # pragma: no cover
 def splinter_webdriver(request):
-  return 'phantomjs'
+    return 'phantomjs'
 
 
 @pytest.fixture(scope='session')
@@ -60,72 +60,70 @@ def splinter_driver_kwargs():
     """Webdriver kwargs."""
     # set resourceTimeout in the hope it will help jenkins kill phantomjs after
     # tests
-    return dict(
-      desired_capabilities={
-        'phantomjs.page.settings.resourceTimeout': '30000', # 30s
-      },
-    )
+    return dict(desired_capabilities={
+        'phantomjs.page.settings.resourceTimeout': '30000',  # 30s
+    },)
 
 
 @pytest.fixture(scope='module')
 def instance_path(request):
-  """
+    """
   creates a temporary directory for instance data
   """
-  tmp_dir = tempfile.mkdtemp(prefix='tmp-pytest-', suffix='-abilian-sbe')
+    tmp_dir = tempfile.mkdtemp(prefix='tmp-pytest-', suffix='-abilian-sbe')
 
-  def clear():
-    shutil.rmtree(tmp_dir)
+    def clear():
+        shutil.rmtree(tmp_dir)
 
-  request.addfinalizer(clear)
-  return tmp_dir
+    request.addfinalizer(clear)
+    return tmp_dir
 
 
 @pytest.fixture(scope='module')
 def app(request, instance_path, app_port):
-  app = Application(instance_path=instance_path)
+    app = Application(instance_path=instance_path)
 
-  # FIXME: need a working environment (DB, Redis...)
-  # with app.app_context():
-  #   app.create_db()
+    # FIXME: need a working environment (DB, Redis...)
+    # with app.app_context():
+    #   app.create_db()
 
-  def worker(app, port):
-    app.run(port=port)
+    def worker(app, port):
+        app.run(port=port)
 
-  process = multiprocessing.Process(target=worker, args=(app, app_port))
+    process = multiprocessing.Process(target=worker, args=(app, app_port))
 
-  try:
-    process.start()
-  except Exception as e:
-    pytest.fail(e.message)
+    try:
+        process.start()
+    except Exception as e:
+        pytest.fail(e.message)
 
-  def finalizer():
-    if process:
-      process.terminate()
+    def finalizer():
+        if process:
+            process.terminate()
 
-  request.addfinalizer(finalizer)
+    request.addfinalizer(finalizer)
 
-  return app
+    return app
 
 
 @skip
 def test_home(browser, app, app_root):
-  browser.visit(app_root)
+    browser.visit(app_root)
 
 
 @skip
 def test_login(browser, app, app_root):
-  browser.visit(app_root + '/user/login')
-  browser.fill('email', "admin@example.com")
-  browser.fill('password', "admin")
-  button = browser.find_by_xpath("//form[@name='login']//button")
-  assert button
+    browser.visit(app_root + '/user/login')
+    browser.fill('email', "admin@example.com")
+    browser.fill('password', "admin")
+    button = browser.find_by_xpath("//form[@name='login']//button")
+    assert button
 
-  # FIXME: at this point this raises an error because we have to set up the db.
-  # button.click()
-  # assert "Welcome to Abilian" in browser.find_element_by_xpath("/html/body").text
+    # FIXME: at this point this raises an error because we have to set up the db.
+    # button.click()
+    # assert "Welcome to Abilian" in browser.find_element_by_xpath("/html/body").text
 
 
 @skip
 def test_forgotten_pw(browser, app, app_root):
-  browser.visit(app_root + '/user/forgotten_pw')
+    browser.visit(app_root + '/user/forgotten_pw')
--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -5,49 +5,52 @@ from abilian.sbe.testing import BaseTestCase
 
 
 class TestViews(BaseTestCase):
-  init_data = True
-  no_login = True
+    init_data = True
+    no_login = True
 
-  def test_all_registered_urls(self):
+    def test_all_registered_urls(self):
 
-    SKIP = frozenset([
-        'admin.audit_search_users',
-        'search.search_main', 'search.live',
-        'notifications.debug_social',
-        'social.groups_json', 'social.groups_new',
-        'social.users_json', 'social.users_dt_json',
-        'communities.community_default_image',
-        'images.user_default',
-    ])
+        SKIP = frozenset([
+            'admin.audit_search_users',
+            'search.search_main',
+            'search.live',
+            'notifications.debug_social',
+            'social.groups_json',
+            'social.groups_new',
+            'social.users_json',
+            'social.users_dt_json',
+            'communities.community_default_image',
+            'images.user_default',
+        ])
 
-    rules = sorted(self.app.url_map.iter_rules(), key=lambda x: x.endpoint)
-    for rule in rules:
+        rules = sorted(self.app.url_map.iter_rules(), key=lambda x: x.endpoint)
+        for rule in rules:
 
-      if "GET" not in rule.methods:
-        continue
+            if "GET" not in rule.methods:
+                continue
 
-      if rule.arguments:
-        continue
+            if rule.arguments:
+                continue
 
-      # FIXME. Skip several exceptions.
-      if rule.endpoint in SKIP:
-        continue
+            # FIXME. Skip several exceptions.
+            if rule.endpoint in SKIP:
+                continue
 
-      # These endpoints expect a parameter ('q') that we don't provide
-      if rule.endpoint.endswith("list_json2"):
-        continue
+            # These endpoints expect a parameter ('q') that we don't provide
+            if rule.endpoint.endswith("list_json2"):
+                continue
 
-      url = url_for(rule.endpoint)
+            url = url_for(rule.endpoint)
 
-      if '/ajax/' in url:
-        continue
+            if '/ajax/' in url:
+                continue
 
-      print(rule.endpoint, url)
+            print(rule.endpoint, url)
 
-      try:
-        response = self.client.get(url)
-        assert response.status_code in (200, 302), \
-          "Bad link: {} (status={})".format(url, response.status_code)
-      except:
-        print("Problem with url: {}".format(url))
-        raise
+            try:
+                response = self.client.get(url)
+                assert response.status_code in (200, 302), \
+                  "Bad link: {} (status={})".format(url, response.status_code)
+            except:
+                print("Problem with url: {}".format(url))
+                raise
