62317fe5891290db4fa012de60ff687c4b06ef47
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-27 23:58:13 -0700

    updating community common.py

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -13,4 +13,7 @@ def object_viewers(current_object):
             tracked_viewers = activitytracker.get_viewers(current_object.id)
             viewers_id = [user.user_id for user in tracked_viewers]
             viewers = filter(lambda user: user.id in list(viewers_id) and user.id != current_object.creator.id, g.community.members)
+            for viewer in viewers:
+                tracked_object = activitytracker.get_tracked_object(current_object.id,viewer.id)[-1]
+                viewer.viewed_at = tracked_object.viewed_at
             return viewers
