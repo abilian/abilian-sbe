63804d34d0e58ee13a5d3bc37171494418c0f421
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-30 01:02:07 -0700

    updating common.py + community macros

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -8,12 +8,15 @@ from flask import current_app, g
 from abilian.services.activitytracker import activitytracker
 
 
-def object_viewers(current_object):
+def object_viewers(obj):
     if current_user.has_role('manager'):
-            tracked_viewers = activitytracker.get_viewers(current_object.id)
-            viewers_id = [user.user_id for user in tracked_viewers]
-            viewers = filter(lambda user: user.id in list(viewers_id) and user.id != current_object.creator.id, g.community.members)
-            for viewer in viewers:
-                tracked_object = activitytracker.get_tracked_object(current_object.id,viewer.id)[-1]
-                viewer.viewed_at = tracked_object.viewed_at
+            tracked_views = activitytracker.get_viewers(obj.id)
+            community_members_dict = {user.id:user for user in g.community.members if user.id != obj.creator.id }
+            viewers = []
+            for view in tracked_views:
+                if view.user_id in set(community_members_dict.keys()):
+                    viewers.append({'user':community_members_dict[view.user_id],'viewed_at':view.viewed_at})
             return viewers
+
+
+
