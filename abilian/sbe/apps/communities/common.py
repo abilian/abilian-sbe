9b87bb537e3c6399d56cee54b5d0a1cab9cc4705
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-30 01:10:29 -0700

    format.

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -11,12 +11,9 @@ from abilian.services.activitytracker import activitytracker
 def object_viewers(obj):
     if current_user.has_role('manager'):
             tracked_views = activitytracker.get_viewers(obj.id)
-            community_members_dict = {user.id:user for user in g.community.members if user.id != obj.creator.id }
+            community_members_dict = {user.id:user for user in g.community.members if user.id != obj.creator.id}
             viewers = []
             for view in tracked_views:
                 if view.user_id in set(community_members_dict.keys()):
                     viewers.append({'user':community_members_dict[view.user_id],'viewed_at':view.viewed_at})
             return viewers
-
-
-
