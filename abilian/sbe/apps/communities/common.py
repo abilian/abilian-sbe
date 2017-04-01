9cdf2859540ac52d54a45aea8035e2197b1f0f42
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-31 20:42:41 -0700

    fix object_viewers

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -10,10 +10,11 @@ from abilian.services.activitytracker import activitytracker
 
 def object_viewers(obj):
     if current_user.has_role('manager'):
-        tracked_views = activitytracker.get_viewers(obj.id)
-        community_members_id = [user.id for user in g.community.members if user.id != obj.creator.id]
-        viewers = []
-        for view in tracked_views:
-            if view.user_id in set(community_members_id):
-                viewers.append({'user':view.user,'viewed_at':view.viewed_at})
-        return viewers
+        if obj:
+            tracked_views = activitytracker.get_viewers(obj.id)
+            community_members_id = [user.id for user in g.community.members if user.id != obj.creator.id]
+            viewers = []
+            for view in tracked_views:
+                if view.user_id in set(community_members_id):
+                    viewers.append({'user':view.user,'viewed_at':view.viewed_at})
+    return viewers
