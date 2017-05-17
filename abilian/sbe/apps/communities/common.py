2b0f006421da43308ca7c8ecd6e47ea076996f83
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-05-17 11:40:44 -0700

    update common.py

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -5,6 +5,7 @@ Forum views
 
 from flask import current_app, g
 from flask_login import current_user
+from datetime import datetime
 
 from abilian.sbe.apps.communities.security import is_manager
 from abilian.services.viewtracker import viewtracker
@@ -25,3 +26,28 @@ def object_viewers(entity):
                     'viewed_at': view.hits[-1].viewed_at
                 })
         return viewers
+
+
+def activity_time_format(time):
+    current_date = datetime.utcnow()
+    time_diffrence = current_date - time
+    month_abbreviation = time.strftime('%B')[:3]
+    days, hours, minutes, seconds = time_diffrence.days, time_diffrence.seconds // 3600, time_diffrence.seconds // 60, time_diffrence.seconds
+
+    if time.year == current_date.year:
+        if time.month == current_date.month:
+            if time.day == current_date.day:
+                if minutes < 1:
+                    return "{}s".format(seconds)
+                elif minutes > 60:
+                    return "{}h".format(hours)
+                else:
+                    return "{}m".format(minutes % 60)
+            elif days == 0:
+                return "{}h".format(hours)
+            else:
+                return "{}d".format(days)
+        else:
+            return "{} {}".format(month_abbreviation, time.day)
+    else:
+        return "{} {}".format(month_abbreviation, str(time.year))
