2e99b10138d176f91b064f9dbd2000fac8668240
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-05-18 15:05:45 -0700

    update common.py activity format function

--- abilian/sbe/apps/communities/common.py
+++ abilian/sbe/apps/communities/common.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from abilian.sbe.apps.communities.security import is_manager
 from abilian.services.viewtracker import viewtracker
 from abilian.i18n import _l
+from flask_babel import format_date
 
 
 def object_viewers(entity):
@@ -32,23 +33,23 @@ def object_viewers(entity):
 def activity_time_format(time):
     current_date = datetime.utcnow()
     time_diffrence = current_date - time
-    month_abbreviation = time.strftime('%B')[:3]
+    month_abbreviation = format_date(time, "MMM")
     days, hours, minutes, seconds = time_diffrence.days, time_diffrence.seconds // 3600, time_diffrence.seconds // 60, time_diffrence.seconds
 
     if time.year == current_date.year:
         if time.month == current_date.month:
             if time.day == current_date.day:
                 if minutes < 1:
-                    return "{}s".format(seconds)
+                    return u"{}s".format(seconds)
                 elif minutes > 60:
-                    return "{}h".format(hours)
+                    return u"{}h".format(hours)
                 else:
-                    return "{}m".format(minutes % 60)
+                    return u"{}m".format(minutes % 60)
             elif days == 0:
-                return "{}h".format(hours)
+                return u"{}h".format(hours)
             else:
-                return "{}{}".format(days,_l(u"d"))
+                return u"{}{}".format(days,_l(u"d"))
         else:
-            return "{} {}".format(month_abbreviation, time.day)
+            return u"{} {}".format(month_abbreviation, time.day)
     else:
-        return "{} {}".format(month_abbreviation, str(time.year))
+        return u"{} {}".format(month_abbreviation, str(time.year))
