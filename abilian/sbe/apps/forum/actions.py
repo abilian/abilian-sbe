24bf3f7de36393e89cfecb8a320addae262756a8
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-05-18 10:46:24 -0700

    update forum actions

--- abilian/sbe/apps/forum/actions.py
+++ abilian/sbe/apps/forum/actions.py
@@ -76,13 +76,6 @@ _actions = (
         _l(u'Attachments'),
         icon='file',
         condition=is_in_thread),
-    ForumAction(
-        'forum:thread',
-        'thread_viewers',
-        _l(u'Readers list'),
-        icon='user',
-        condition=lambda ctx: is_manager(context=ctx),
-        url="viewers"),
     ForumModalAction(
         'forum:thread',
         'delete',
