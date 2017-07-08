8ae97f5e336d3833198aef239e79f5715ee78acd
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-07-07 17:19:02 -0700

    add gallery js to extentions

--- abilian/sbe/extension.py
+++ abilian/sbe/extension.py
@@ -20,6 +20,7 @@ JS = (
     'js/jquery.fileapi.js',
     'js/folder.js',
     'js/folder_edit.js',
+    'js/folder_gallery.js',
     'js/folder_upload.js',
     'js/document_viewer.js',
     'js/bootstrap-tagsinput.js',)
