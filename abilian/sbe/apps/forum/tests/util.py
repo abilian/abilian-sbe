e8f12a46a96d2eeebcf238f30d900f77c6d39bf3
Stefane Fermigier <sf@fermigier.com>
2018-02-19 17:21:37 +0100

    fix pylint --py3k error.

--- abilian/sbe/apps/forum/tests/util.py
+++ abilian/sbe/apps/forum/tests/util.py
@@ -1,4 +1,5 @@
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
 
 from email.feedparser import FeedParser
 from pathlib import Path
