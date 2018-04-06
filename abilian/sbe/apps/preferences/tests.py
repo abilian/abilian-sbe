4e27e55dd39c2cac0f0c0d2c489413494fb46183
Stefane Fermigier <sf@fermigier.com>
2018-04-06 20:18:16 +0200

    deps

--- abilian/sbe/apps/preferences/tests.py
+++ abilian/sbe/apps/preferences/tests.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 from flask import url_for
 
 
-def test_crm_notifications(client, login_user, req_ctx):
+def test_sbe_notifications(client, login_user, req_ctx):
     response = client.get(url_for("preferences.sbe_notifications"))
     assert response.status_code == 200
