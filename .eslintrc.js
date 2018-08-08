ad8152b235436de3a657e2c7e99a944aa4eedec0
Stefane Fermigier <sf@fermigier.com>
2018-08-08 20:04:28 +0200

    tweak eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
   root: true,
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: [
-    'standard',
-    "plugin:requirejs/recommended",
-  ],
+  extends: ["standard", "plugin:requirejs/recommended"],
   // required to lint *.vue files
-  plugins: [
-    'html', 'requirejs'
-  ],
+  plugins: ["html", "requirejs"],
   env: {
     browser: true,
     es6: true,
   },
   // add your custom rules here
   rules: {
-    'quotes': 'off',
-    'semi': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'space-before-function-paren': 'off',
-    'camelcase': 'off',
-    'curly': 'error',
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "space-before-function-paren": ["error", "never"],
+    camelcase: "off",
+    curly: "error",
   },
 };
