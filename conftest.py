5fe46a075d1a4838655ac3226be4337aa4a24efd
Stefane Fermigier <sf@fermigier.com>
2019-06-06 17:11:45 +0200

    Runtime typechecker.

--- conftest.py
+++ conftest.py
@@ -3,8 +3,12 @@
 
 Reuses fixtures defined in abilian-core.
 """
+import logging
+import os
+
 from abilian.testing.fixtures import TestConfig
 from pytest import fixture
+from typeguard import TypeChecker
 
 from abilian.sbe.app import create_app
 
@@ -12,6 +16,13 @@ pytest_plugins = [
     "abilian.testing.fixtures",
     "abilian.sbe.apps.communities.tests.fixtures",
 ]
+
+
+checker = TypeChecker("abilian")
+if "TYPECHECK" in os.environ:
+    logging.captureWarnings(True)
+    if not checker.active:
+        checker.start()
 
 
 class NoCsrfTestConfig(TestConfig):
