d4ede3dcddfed91351f87ac4943d536ad5b32e83
Stefane Fermigier <sf@fermigier.com>
2020-11-30 10:57:00 +0100

    deps

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name="abilian-sbe",
-    version="0.5.15",
+    version="0.5.16",
     description="Social Business platform, including: document management, wiki, forum, enterprise social networking, and more",
     python_requires="<4,>=3.6.1",
     project_urls={"repository": "https://github.com/abilian/abilian-sbe"},
